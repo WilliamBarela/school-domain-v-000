f005c0f1398e1ae1d0fcb50d1b1067adc0ab81b9
William <william.barela.dev@gmail.com>
2017-03-27 18:39:35 +0900

    Solution Error
    Correcting an error in the solution where roster is treated as a local variable instead of an instance variable. Also providing a more succinct solution for sort.

--- lib/school.rb
+++ lib/school.rb
@@ -1,26 +1,24 @@
 class School
-  attr_accessor :name, :roster
+  attr_accessor :school
+  attr_reader :roster
 
-  def initialize(name)
-    @name = name
+  def initialize(school)
+    @school = school
     @roster = {}
   end
 
-  def add_student(student_name, grade)
-    roster[grade] ||= []
-    roster[grade] << student_name
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
   end
 
-  def grade(student_grade)
-    roster[student_grade]
+  def grade(grade)
+    @roster[grade]
   end
-
+  
   # this method should arrange the students in each grade by alphabetical order
   def sort
-    sorted = {}
-    roster.each do |grade, students|
-      sorted[grade] = students.sort
-    end
-    sorted
+    @roster.each{|grade, subroster| subroster.sort!}
   end
+
 end
